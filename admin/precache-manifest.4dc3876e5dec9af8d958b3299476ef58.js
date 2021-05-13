4514fcb78ea2b6eb76fa62265f0cde1d294a0928
juancarlos318504 <43257439+juancarlos318504@users.noreply.github.com>
2021-05-13 12:22:50 -0500

    Add files via upload

---
+++ admin/precache-manifest.4dc3876e5dec9af8d958b3299476ef58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab02e77871f72367dbb080f580948bb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "062c758efa21d3fae526",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c7f87254f8ff0dc4d8c",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "062c758efa21d3fae526",
+    "url": "./static/js/2.e62ccfe5.chunk.js"
+  },
+  {
+    "revision": "83cf22677282ef599fd4eb17fb28d692",
+    "url": "./static/js/2.e62ccfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7f87254f8ff0dc4d8c",
+    "url": "./static/js/main.b7c9e2fd.chunk.js"
+  },
+  {
+    "revision": "fc5fc6c3961f57201e3a",
+    "url": "./static/js/runtime-main.6dc1a488.js"
+  }
+]);
